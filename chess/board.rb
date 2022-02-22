dbd6db78a4c8f091f094122f6a44ff043e81a860
App Academy Student <email@email.com>
2022-02-22 14:16:26 -0500

    stuck on board

--- chess/board.rb
+++ chess/board.rb
@@ -1,7 +1,23 @@
+require_relative 'piece.rb'
+require 'byebug'
+
 class Board
-  
+  attr_reader :board
+
   def initialize
     @board = Array.new(8) { Array.new(8) }
+    build_board
+    debugger
+  end
+  
+  def build_board
+    @board.map.with_index do |row, idx|
+      if idx == 0 || idx == 1 || idx == 6 || idx == 7
+        return row.map { |ele| Piece.new }
+        # debugger
+        # p row
+      end
+    end
   end
 
   def [](pos)
@@ -14,3 +30,5 @@ class Board
   end
 
 end
+
+b = Board.new
