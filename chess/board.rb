4b93706a8639a94bc7eef0b0f3e4113959498dac
App Academy Student <email@email.com>
2022-02-22 12:16:19 -0500

    lunch, created chess skeleton

---
+++ chess/board.rb
@@ -0,0 +1,16 @@
+class Board
+  
+  def initialize
+    @board = Array.new(8) { Array.new(8) }
+  end
+
+  def [](pos)
+  end
+
+  def []=(pos)
+  end
+
+  def move_piece(start_pos, end_pos)
+  end
+
+end
